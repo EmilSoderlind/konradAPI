8950f99d39d56834bdf43768e9dc101c8320140b
Emil Söderlind <id15esd@cs.umu.se>
2019-04-13 18:11:47 +0200

    Added (commented) delete functionality

--- app.js
+++ app.js
@@ -18,6 +18,40 @@ app.get('/', (req, res) => {
     }
 })
 
+/*
+app.get('/removeID/:id', function (req, res) {
+    console.log("Remove request on ID: " + req.params.id)
+    var idToRemove = req.params.id
+    
+    fs.readFile(filename, 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            
+            obj = JSON.parse(data); //now it an object
+            var length = Object.keys(obj.mailRecievedTable).length
+            
+            for(var i = 0; i<length; i++){
+                console.log(obj.mailRecievedTable[i])
+                if (obj.mailRecievedTable[i].id == req.params.id){
+
+                    delete obj.mailRecievedTable[i]
+
+                    json = JSON.stringify(obj); //convert it back to json
+
+                    fs.writeFileSync(filename, json, 'utf8'); // write it back 
+
+                    console.log("Removed")
+                    res.send('Removed ID: ' + req.params.id);
+                }
+
+            }
+
+        }
+    });
+});
+*/
+
 app.listen(port, () => {
     console.log(`KonradAPI listening on port ${port}!`)
 })
