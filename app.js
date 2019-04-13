3e1066b89664682c78910400180fd0d7ab8159a2
Emil Söderlind <id15esd@cs.umu.se>
2019-04-13 16:19:17 +0200

    updating routing

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var fs = require('fs');
+const port = 313
+const filename = process.argv[2]
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    
+    try {
+        let rawdata = fs.readFileSync(filename)
+        let mailData = JSON.parse(rawdata)
+        res.send(mailData)
+    }catch (error) {
+        console.error(error);
+        res.send("Could not open file.")
+        console.error("Could not open file: " + filename)
+        process.exit()
+    }
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log(`KonradAPI listening on port ${port}!`)
+})
